3425d3fc26570e5fdad12fe9d44ad51191d222a4
Alex <sedaghatplus@gmail.com>
2026-06-04 10:01:23 +0330

    login should be in bash

--- login.py
+++ login.py
@@ -1,3 +1,18 @@
-print("Hello world!")
-
 print("this is a program for testing a git module")
+
+import socket
+
+def check_internet():
+    try:
+        # Try to connect to Google's DNS server
+        socket.setdefaulttimeout(3)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+        return True
+    except (socket.error, OSError):
+        return False
+
+# Check and print result
+if check_internet():
+    print("YES")
+else:
+    print("NO")
