afbc08a172e53713fdfd0a17eaa5446ed8e25b47
Alex <sedaghatplus@gmail.com>
2026-06-06 17:46:05 +0330

    create a program by bash to login to a website

--- login.py
+++ login.py
@@ -1,7 +1,25 @@
-# create a program by python to login to a website
+# create a program by python to login to a website in bash
+
 import requests
+import json
+import os
+import sys
+import time
+import random
+import string
+import datetime
+import threading
+import subprocess
 
-url = "https://www.google.com"
-response = requests.get(url)
-print(response.text)
+def login(username, password):
+    url = "https://www.google.com"
+    response = requests.get(url)
+    print(response.text)
 
+if __name__ == "__main__":
+    login("username", "password")
+    login("username", "password")
+    login("username", "password")       
+
+
+    
