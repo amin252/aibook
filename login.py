00b751f2302bfef9be30d898e8a6209c5f9e3def
Alex <sedaghatplus@gmail.com>
2026-06-06 17:43:48 +0330

    write a program to loging to the website by python

--- login.py
+++ login.py
@@ -1,18 +1,7 @@
-print("this is a program for testing a git module")
+# create a program by python to login to a website
+import requests
 
-import socket
+url = "https://www.google.com"
+response = requests.get(url)
+print(response.text)
 
-def check_internet():
-    try:
-        # Try to connect to Google's DNS server
-        socket.setdefaulttimeout(3)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
-        return True
-    except (socket.error, OSError):
-        return False
-
-# Check and print result
-if check_internet():
-    print("YES")
-else:
-    print("NO")
